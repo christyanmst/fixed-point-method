eea501596f27a85f2fe4dfa781264822f6ca2c04
christyanmst <christyan228@gmail.com>
2022-04-23 01:50:07 -0300

    Update calendario.py

--- calendario.py
+++ calendario.py
@@ -1,9 +1,11 @@
 import calendar
-   
+
 x = int(input('Digite um ano para o calendário:\n'))
 y = int(input('Digite um mês para o calendário:\n'))
 
 z= calendar.month(x,y)
+i = calendar.calendar(x, 2, 1, 6)
 
-print(z)
+print("\nO ",y,"º mês de ",x," é:\n",z)
 
+print("O calendário do ano de ",x, " é: ", i)
