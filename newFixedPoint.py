7f30ded946169508393ff8b93475f76cd8d4eae4
christyanmst <christyan228@gmail.com>
2022-05-11 21:11:54 -0300

    Update newFixedPoint.py

--- newFixedPoint.py
+++ newFixedPoint.py
@@ -1,12 +1,11 @@
-from numpy import*
+from numpy import *
 import math
-
-print("Método do Ponto Fixo!")
-a   = float(input("Informe o primeiro intervalo:\n"))
-b   = float(input("Informe o segundo intervalo:\n"))
-err = float(input("Informe o valor do erro em decimal:\n"))
-y1  = input("Informe a função f(x):\n")
-y2  = input("Informe a função g(x):\n")
+print("Método do Ponto Fixo")
+a = float(input("Informe o primeiro intervalo:\n"))
+b = float(input("Informe o segundo intervalo:\n"))
+e=float(input("Entre com o numero real e:\n"))
+y1 = input("Informe a função f(x):\n")
+y2 = input("Informe a função g(x):\n")
 funcao = str(y1).replace("**", "^").replace("*","")
 
 def f(x):
@@ -16,13 +15,20 @@ def g(x):
     z = eval(y2)
     return z 
 
-X_m = (a+b)/2
-k = 0
-
-while abs(f(X_m))>err:
-    k = k+1
-    X_m = g(X_m)
-print("\nA raiz para a função: ",funcao," é aprox. x=",X_m,'com',k,' iterações' )
+def fixedPointIteration():
+    if( b>a):
+        x0=(a+b)/2
+        i=1
+        while True:
+            x1=g(x0)
+            print('Iteração-%d, x= %0.6f e f(x) = %0.6f' % (i, x1, f(x1)))
+            
+            if abs(f(x1)) < e or abs(abs(x1)-abs(x0)) <e:
+                print("\nFunção : %s , Número de iterações = %d, x = %0.6f e f(x) = %0.6f" % (funcao, i, x1, f(x1)))
+                break   
+            i+=1
+            x0=x1
+fixedPointIteration()
 
 # 1° exemplo 
 # f(x)= x^2 -6x +3
